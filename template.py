eea83c9acfb3c6e7c5d586bc01a19dc8a873c666
Jillani Soft Tech <76724558+MGJillaniMughal@users.noreply.github.com>
2023-10-28 00:57:44 -0700

    requirements added

--- template.py
+++ template.py
@@ -23,7 +23,8 @@ list_of_files = [
     'params.yaml',
     'requirements.txt',
     'setup.py',
-    'research/trials.ipynb'
+    'research/trials.ipynb',
+    'templates/index.html',
 ]
 
 for filepath in list_of_files:
