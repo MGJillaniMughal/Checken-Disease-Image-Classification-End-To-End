673da07cc7f3030c64d44b0d33379c8fad1d2a0e
Jillani Soft Tech <76724558+MGJillaniMughal@users.noreply.github.com>
2023-10-28 05:16:40 -0700

    Data Ingestion Completed

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # Checken-Disease-Image-Classification-End-To-End
-Checken Disease Image Classification End To End
+
+# Chicken-Disease-Classification--Project
+
+# Workflows
+
+
+    1. Update config.yaml
+    2. Update secrets.yaml [Optional]
+    3. Update params.yaml
+    4. Update the entity
+    5. Update the configuration manager in src config
+    6. Update the components
+    7. Update the pipeline
+    8. Update the main.py
+    9. Update the dvc.yaml
+
+
+
---
+++ main.py
@@ -0,0 +1,64 @@
+from CnnClassifier import logger
+from CnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from CnnClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
+from CnnClassifier.pipeline.stage_03_training import ModelTrainingPipeline
+from CnnClassifier.pipeline.stage_04_evaluation import EvaluationPipeline
+
+
+STAGE_NAME = "Data Ingestion stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+
+
+STAGE_NAME = "Prepare base model"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   prepare_base_model = PrepareBaseModelTrainingPipeline()
+   prepare_base_model.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+
+
+STAGE_NAME = "Training"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   model_trainer = ModelTrainingPipeline()
+   model_trainer.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+
+
+
+
+STAGE_NAME = "Evaluation stage"
+try:
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   model_evalution = EvaluationPipeline()
+   model_evalution.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+
